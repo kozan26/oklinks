f72a515d1b25b09c9e4b34bb6a3c63dccb41de00
Ozan Kılıç <kozan26@gmail.com>
2025-11-02 04:38:54 +0300

    Clear repository for fresh start

--- functions/types.ts
+++
@@ -1,9 +0,0 @@
-export interface CloudflarePagesEnv {
-  DB?: D1Database;
-  CACHE?: KVNamespace;
-  CLICK_QUEUE?: Queue;
-  TURNSTILE_SECRET?: string;
-  ACCESS_AUD?: string;
-  ASSETS?: Fetcher; // For serving static files in Pages Functions
-}
-
